6e991440d9b0c33d1ff9055d40ea861078e87952
Friedinger <71267611+Friedinger@users.noreply.github.com>
2026-05-15 11:34:11 +0200

    🎉 Setup backend

---
+++ ecoscan_backend/src/test/java/com/rubberduckcrew/ecoscan_backend/EcoscanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rubberduckcrew.ecoscan_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoscanBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
