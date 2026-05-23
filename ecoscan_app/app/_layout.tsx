555cf97a1656d827cbd9df177bc5b5d080f6801a
hm-sbartl <sophie.bartl1@hm.edu>
2026-05-23 09:17:37 +0200

    :bug: fixed import

--- ecoscan_app/app/_layout.tsx
+++ ecoscan_app/app/_layout.tsx
@@ -1,6 +1,6 @@
 import {Stack} from 'expo-router';
 import {PaperProvider} from 'react-native-paper';
-import {theme} from './theme';
+import {theme} from '../theme';
 
 export default function RootLayout() {
     return (
