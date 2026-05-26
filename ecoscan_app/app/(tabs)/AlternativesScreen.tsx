1b810a95c28f8817d8e20201595082c02c143891
hm-sbartl <sophie.bartl1@hm.edu>
2026-05-26 16:00:41 +0200

    :sparkles: added screen with dummy data and mobile feature

--- ecoscan_app/app/(tabs)/AlternativesScreen.tsx
+++ ecoscan_app/app/(tabs)/AlternativesScreen.tsx
@@ -1,8 +1,59 @@
-import {View, StyleSheet} from "react-native";
-import {Text} from "react-native-paper";
+import {FlatList, StyleSheet} from "react-native";
+import {Surface, Text} from "react-native-paper";
 import ProductCard from "@/components/alternatives/ProductCard";
+import AlternativeCard from "@/components/alternatives/AlternativeCard";
 
 export default function AlternativesScreen() {
+    const alternatives = [
+        {
+            title: "Alternativprodukt",
+            image: "../assets/images/icon.png",
+            scanScore: 87,
+            alternativeScore: 95,
+            targetLatitude: 48.15520,
+            targetLongitude: 11.55457,
+        },
+        {
+            title: "Alternativprodukt2",
+            image: "../assets/images/icon.png",
+            scanScore: 87,
+            alternativeScore: 90,
+            targetLatitude: 48,
+            targetLongitude: 10,
+        },
+        {
+            title: "Alternativprodukt3",
+            image: "../assets/images/icon.png",
+            scanScore: 87,
+            alternativeScore: 88,
+            targetLatitude: 48.03655106953577,
+            targetLongitude: 10.727155318771056,
+        },
+        {
+            title: "Alternativprodukt",
+            image: "../assets/images/icon.png",
+            scanScore: 87,
+            alternativeScore: 95,
+            targetLatitude: 48.15520,
+            targetLongitude: 11.55457,
+        },
+        {
+            title: "Alternativprodukt2",
+            image: "../assets/images/icon.png",
+            scanScore: 87,
+            alternativeScore: 90,
+            targetLatitude: 48,
+            targetLongitude: 10,
+        },
+        {
+            title: "Alternativprodukt3",
+            image: "../assets/images/icon.png",
+            scanScore: 87,
+            alternativeScore: 88,
+            targetLatitude: 48.03655106953577,
+            targetLongitude: 10.727155318771056,
+        },
+    ];
     const defaultProduct = {
         title: "Titel des Produkts",
         description: "Beschreibung\nBeschreibung\nBeschreibung",
@@ -10,7 +61,7 @@ export default function AlternativesScreen() {
         score: 87,
     }
     return (
-        <View style={styles.pageStyle}>
+        <Surface style={styles.pageStyle}>
             <Text variant="headlineMedium" style={styles.headline}>
                 Bessere Alternativen
             </Text>
@@ -18,7 +69,19 @@ export default function AlternativesScreen() {
                 In deiner Nähe verfügbar
             </Text>
             <ProductCard {...defaultProduct} />
-        </View>
+            <FlatList
+                data={alternatives.sort((a, b) => b.alternativeScore - a.alternativeScore)}
+                renderItem={({ item }) =>
+                    <AlternativeCard
+                        title={item.title}
+                        image={item.image}
+                        scanScore={item.scanScore}
+                        alternativeScore={item.alternativeScore}
+                        targetLatitude={item.targetLatitude}
+                        targetLongitude={item.targetLongitude}
+                    />}
+            />
+        </Surface>
     );
 }
 
