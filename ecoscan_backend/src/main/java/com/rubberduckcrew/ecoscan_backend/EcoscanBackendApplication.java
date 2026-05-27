3865a96d358cbdf5eff1a442c53f98ea2d7c8ebe
Friedinger <71267611+Friedinger@users.noreply.github.com>
2026-05-28 00:06:55 +0200

    :bug: Exclude Mongo DB temporarily until it is needed

--- ecoscan_backend/src/main/java/com/rubberduckcrew/ecoscan_backend/EcoscanBackendApplication.java
+++ ecoscan_backend/src/main/java/com/rubberduckcrew/ecoscan_backend/EcoscanBackendApplication.java
@@ -2,12 +2,17 @@ package com.rubberduckcrew.ecoscan_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.mongodb.autoconfigure.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = {
+                MongoAutoConfiguration.class, // TODO: Remove on mongo db integration
+        }
+)
 public class EcoscanBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EcoscanBackendApplication.class, args);
-	}
+    static void main(String[] args) {
+        SpringApplication.run(EcoscanBackendApplication.class, args);
+    }
 
 }
