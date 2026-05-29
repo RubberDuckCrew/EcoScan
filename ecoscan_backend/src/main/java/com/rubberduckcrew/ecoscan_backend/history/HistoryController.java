bbcce0452bf85e0c05a745c3c238ba3521ee8589
Friedinger <71267611+Friedinger@users.noreply.github.com>
2026-05-29 23:46:26 +0200

    :safety_vest: Validate history page

--- ecoscan_backend/src/main/java/com/rubberduckcrew/ecoscan_backend/history/HistoryController.java
+++ ecoscan_backend/src/main/java/com/rubberduckcrew/ecoscan_backend/history/HistoryController.java
@@ -4,6 +4,7 @@ import com.rubberduckcrew.ecoscan_backend.common.SliceDTO;
 import com.rubberduckcrew.ecoscan_backend.history.dto.ScanHistoryDTO;
 import com.rubberduckcrew.ecoscan_backend.history.entity.ScanHistory;
 import com.rubberduckcrew.ecoscan_backend.utils.AuthUtils;
+import jakarta.validation.constraints.Min;
 import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +12,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.Sort;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,7 +26,8 @@ public class HistoryController {
     private final HistoryMapper historyMapper;
 
     @GetMapping
-    public SliceDTO<ScanHistoryDTO> getHistory(@RequestParam(defaultValue = "0") final int page) {
+    @Validated
+    public SliceDTO<ScanHistoryDTO> getHistory(@RequestParam(defaultValue = "0") @Min(0) final int page) {
         final UUID userId = AuthUtils.getSub();
         final Pageable pageable = PageRequest.of(page, 10, Sort.by("savedDate").descending());
         final Slice<ScanHistory> history = historyService.getUserHistory(userId, pageable);
