2cadb3a59088d7288072eb2cd05612036bcc06df
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2026-05-31 09:29:17 +0200

    :sparkles: Add API client configuration and score product functionality

---
+++ ecoscan_backend/src/main/java/com/rubberduckcrew/ecoscan_backend/score/ScoreService.java
@@ -0,0 +1,43 @@
+package com.rubberduckcrew.ecoscan_backend.score;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+import com.rubberduckcrew.ecoscan_backend.products.ProductRepository;
+import com.rubberduckcrew.ecoscan_backend.products.entity.Product;
+import com.rubberduckcrew.ecoscanai.api.GreenScoreApi;
+import com.rubberduckcrew.ecoscanai.model.JobResponse;
+import com.rubberduckcrew.ecoscanai.model.ScoreProductRequest;
+import java.util.Optional;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+@RequiredArgsConstructor
+public class ScoreService {
+
+    private final ProductRepository productRepository;
+
+    private final GreenScoreApi greenScoreApi;
+
+    public UUID scoreProduct(final String id) {
+
+        final Product product = productRepository.findById(id).orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "Product not found"));
+        final ScoreProductRequest scoreProductRequest = new ScoreProductRequest();
+        scoreProductRequest.setProductContext(product.getDescription());
+        final Optional<JobResponse> jobResponse;
+
+        try {
+            jobResponse = Optional.of(greenScoreApi.scoreProduct(scoreProductRequest));
+        } catch (ResponseStatusException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to score product", e);
+        }
+
+        return jobResponse.map(JobResponse::getJobId).orElseThrow(() -> new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
+                "Failed to score product"));
+
+    }
+
+}
