aacd08dd4ed92ee96be22ed4a397a3ceb3acba36
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2026-05-31 20:30:45 +0200

    :goal_net: Enhance ScoreService to handle validation and REST client exceptions with logging

--- ecoscan_backend/src/main/java/com/rubberduckcrew/ecoscan_backend/score/ScoreService.java
+++ ecoscan_backend/src/main/java/com/rubberduckcrew/ecoscan_backend/score/ScoreService.java
@@ -5,15 +5,19 @@ import com.rubberduckcrew.ecoscan_backend.products.entity.Product;
 import com.rubberduckcrew.ecoscanai.api.GreenScoreApi;
 import com.rubberduckcrew.ecoscanai.model.JobResponseGreenScoreResult;
 import com.rubberduckcrew.ecoscanai.model.ScoreProductRequest;
+import jakarta.validation.ValidationException;
 import java.util.Optional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ScoreService {
 
     private final ProductService productService;
@@ -29,7 +33,11 @@ public class ScoreService {
 
         try {
             jobResponse = Optional.of(greenScoreApi.scoreProduct(scoreProductRequest));
-        } catch (ResponseStatusException e) {
+        } catch (ValidationException e) {
+            log.error("OpenAPI client error while scoring product", e);
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to score product", e);
+        } catch (RestClientException e) {
+            log.error("REST client error while scoring product", e);
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to score product", e);
         }
 
