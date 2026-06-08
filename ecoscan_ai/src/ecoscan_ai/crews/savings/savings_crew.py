525654d1081534e94c0d63ec94bc1e27bcfffcdd
Friedinger <71267611+Friedinger@users.noreply.github.com>
2026-06-08 13:04:24 +0200

    :sparkles: Define savings crew

--- ecoscan_ai/src/ecoscan_ai/crews/savings/savings_crew.py
+++ ecoscan_ai/src/ecoscan_ai/crews/savings/savings_crew.py
@@ -8,44 +8,51 @@ from ecoscan_ai.api.schemas.savings_result import SavingsResult
 class SavingsCrew:
     @agent
     def evaluator(self) -> Agent:
-        return Agent(config=self.agents_config["evaluator"], verbose=True)  # type: ignore[index]
+        return Agent(
+            config=self.agents_config["evaluator"],
+            verbose=True
+        )
 
     @agent
     def score_agent(self) -> Agent:
-        return Agent(config=self.agents_config["score_agent"], verbose=True)  # type: ignore[index]
+        return Agent(
+            config=self.agents_config["score_agent"],
+            verbose=True
+        )
 
     @agent
     def reasoning_agent(self) -> Agent:
         return Agent(
-            config=self.agents_config["reasoning_agent"], reasoning=True, verbose=True
-        )  # type: ignore[index]
+            config=self.agents_config["reasoning_agent"],
+            reasoning=True,
+            verbose=True
+        )
 
     @task
     def evaluate_task(self) -> Task:
-        return Task(config=self.tasks_config["evaluate_task"])  # type: ignore[index]
+        return Task(
+            config=self.tasks_config["evaluate_task"]
+        )
 
     @task
     def score_task(self) -> Task:
         return Task(
-            config=self.tasks_config["score_task"],  # type: ignore[index]
+            config=self.tasks_config["score_task"]
         )
 
     @task
     def reasoning_task(self) -> Task:
         return Task(
             config=self.tasks_config["reasoning_task"],
-            output_pydantic=SavingsResult,  # type: ignore[index]
+            output_pydantic=SavingsResult
         )
 
     @crew
     def crew(self) -> Crew:
         """Creates the Savings crew"""
-        # To learn how to add knowledge sources to your crew, check out the documentation:
-        # https://docs.crewai.com/concepts/knowledge#what-is-knowledge
-
         return Crew(
-            agents=self.agents,  # Automatically created by the @agent decorator
-            tasks=self.tasks,  # Automatically created by the @task decorator
+            agents=self.agents,
+            tasks=self.tasks,
             process=Process.sequential,
             verbose=True,
         )
