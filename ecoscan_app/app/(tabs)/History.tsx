09a420823d35d87dee258015511ac3ddca9fd010
Friedinger <71267611+Friedinger@users.noreply.github.com>
2026-06-08 18:11:35 +0200

    :recycle: Extract history list to own component

--- ecoscan_app/app/(tabs)/History.tsx
+++ ecoscan_app/app/(tabs)/History.tsx
@@ -1,33 +1,10 @@
 import React from "react";
-import { FlatList, StyleSheet, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { Text } from "react-native-paper";
 import { PageContainer } from "@/components/PageContainer";
-import HistoryListItem from "@/components/HistoryListItem";
-import { useHistory } from "@/hooks/useHistory";
-import type { HistoryItem } from "@/types/history";
-import { LoadingIndicator } from "@/components/LoadingIndicator";
+import { HistoryList } from "@/components/HistoryList";
 
 export default function History() {
-  const { history, loading, loadNext, refresh, refreshing } = useHistory();
-
-  const renderItem = ({ item }: { item: HistoryItem }) => (
-    <HistoryListItem item={item} />
-  );
-
-  const ListFooter = () =>
-    loading &&
-    history.length > 0 && (
-      <View style={styles.footer}>
-        <LoadingIndicator />
-      </View>
-    );
-
-  const ListEmpty = () => (
-    <View style={styles.emptyContainer}>
-      {loading ? <LoadingIndicator /> : <Text>Keine Historie vorhanden</Text>}
-    </View>
-  );
-
   return (
     <PageContainer style={{ padding: 0 }}>
       <View style={styles.wrapper}>
@@ -37,22 +14,7 @@ export default function History() {
         <Text variant="headlineSmall" style={styles.heading}>
           Scanverlauf
         </Text>
-        <FlatList
-          data={history}
-          keyExtractor={(item) => item.id}
-          renderItem={renderItem}
-          onEndReached={loadNext}
-          onEndReachedThreshold={0.5}
-          refreshing={refreshing}
-          onRefresh={refresh}
-          ListFooterComponent={ListFooter}
-          ListEmptyComponent={ListEmpty}
-          contentContainerStyle={
-            history.length === 0
-              ? styles.emptyContainer
-              : styles.filledContainer
-          }
-        />
+        <HistoryList />
       </View>
     </PageContainer>
   );
@@ -65,20 +27,4 @@ const styles = StyleSheet.create({
     gap: 8,
   },
   heading: { paddingInline: 16, fontWeight: "bold" },
-  footer: {
-    paddingVertical: 16,
-    paddingBottom: 16,
-    alignItems: "center",
-  },
-  emptyContainer: {
-    flex: 1,
-    padding: 16,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  filledContainer: {
-    paddingHorizontal: 16,
-    paddingBottom: 8,
-    gap: 8,
-  },
 });
---
+++ ecoscan_app/components/HistoryList.tsx
@@ -0,0 +1,65 @@
+import { FlatList, StyleSheet, View } from "react-native";
+import { Text } from "react-native-paper";
+import React from "react";
+import { LoadingIndicator } from "@/components/LoadingIndicator";
+import { useHistory } from "@/hooks/useHistory";
+import { HistoryItem } from "@/types/history";
+import HistoryListItem from "@/components/HistoryListItem";
+
+export function HistoryList() {
+  const { history, loading, loadNext, refresh, refreshing } = useHistory();
+
+  const renderItem = ({ item }: { item: HistoryItem }) => (
+    <HistoryListItem item={item} />
+  );
+
+  const ListFooter = () =>
+    loading &&
+    history.length > 0 && (
+      <View style={styles.footer}>
+        <LoadingIndicator />
+      </View>
+    );
+
+  const ListEmpty = () => (
+    <View style={styles.emptyContainer}>
+      {loading ? <LoadingIndicator /> : <Text>Keine Historie vorhanden</Text>}
+    </View>
+  );
+
+  return (
+    <FlatList
+      data={history}
+      keyExtractor={(item) => item.id}
+      renderItem={renderItem}
+      onEndReached={loadNext}
+      onEndReachedThreshold={0.5}
+      refreshing={refreshing}
+      onRefresh={refresh}
+      ListFooterComponent={ListFooter}
+      ListEmptyComponent={ListEmpty}
+      contentContainerStyle={
+        history.length === 0 ? styles.emptyContainer : styles.filledContainer
+      }
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  footer: {
+    paddingVertical: 16,
+    paddingBottom: 16,
+    alignItems: "center",
+  },
+  emptyContainer: {
+    flex: 1,
+    padding: 16,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  filledContainer: {
+    paddingHorizontal: 16,
+    paddingBottom: 8,
+    gap: 8,
+  },
+});
