f25244fef3b6b2136d8682778049a75672853a59
hm-sbartl <sophie.bartl1@hm.edu>
2026-06-11 09:56:12 +0200

    :bug: fixed navigation bar and app bar for alternatives screen

--- ecoscan_app/app/(tabs)/_layout.tsx
+++ ecoscan_app/app/(tabs)/_layout.tsx
@@ -40,6 +40,12 @@ export default function TabLayout() {
           }}
         />
       ))}
+        <Tabs.Screen
+            name="alternatives/[product]"
+            options={{
+                href: null,
+            }}
+        />
     </Tabs>
   );
 }
