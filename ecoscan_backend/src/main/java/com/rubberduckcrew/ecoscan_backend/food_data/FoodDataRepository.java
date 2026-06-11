30c021e8472441607a7b0da28188f81f942a3957
Ruby <152699064+Ruby11235@users.noreply.github.com>
2026-06-11 13:33:38 +0200

    :sparkles: import database

--- ecoscan_backend/src/main/java/com/rubberduckcrew/ecoscan_backend/food_data/FoodDataRepository.java
+++ ecoscan_backend/src/main/java/com/rubberduckcrew/ecoscan_backend/food_data/FoodDataRepository.java
@@ -4,15 +4,29 @@ import com.rubberduckcrew.ecoscan_backend.configuration.FoodDataTemplate;
 import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.server.ResponseStatusException;
 
 @Repository
 @RequiredArgsConstructor
 public class FoodDataRepository {
     private final FoodDataTemplate foodDataTemplate;
 
-    public List<Map<String, Object>> queryParquetData() {
-        final String sql = "SELECT code FROM food LIMIT 10";
-        return foodDataTemplate.queryForList(sql);
+    @Value("${food-data.parquet-path}")
+    private String parquetPath;
+
+    public Map<String, Object> getProduct(final String id) {
+        //TODO nicht alle Spalten
+        final String sql = "SELECT * FROM read_parquet(?) WHERE code = ?";
+        List<Map<String, Object>> results = foodDataTemplate.queryForList(sql, parquetPath, id);
+
+        if (results.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    "Product " + id + " not found");
+        }
+
+        return results.getFirst();
     }
 }
--- ecoscan_backend/src/main/java/com/rubberduckcrew/ecoscan_backend/products/ProductService.java
+++ ecoscan_backend/src/main/java/com/rubberduckcrew/ecoscan_backend/products/ProductService.java
@@ -1,5 +1,6 @@
 package com.rubberduckcrew.ecoscan_backend.products;
 
+import com.rubberduckcrew.ecoscan_backend.food_data.FoodDataRepository;
 import com.rubberduckcrew.ecoscan_backend.products.entity.Product;
 import com.rubberduckcrew.ecoscan_backend.products.entity.ScannedProduct;
 import com.rubberduckcrew.ecoscanai.model.GreenScoreResult;
@@ -9,6 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Map;
+
 @Service
 @RequiredArgsConstructor
 public class ProductService {
@@ -16,12 +19,35 @@ public class ProductService {
     private final ProductRepository productRepository;
     private final ScannedProductRepository scannedProductRepository;
     private final ProductMapper productMapper;
+	private final FoodDataRepository foodDataRepository;
 
     public Product getProduct(final String id) {
-        return productRepository.getProductById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+        return productRepository.getProductById(id).orElseThrow(() -> new
+		 ResponseStatusException(HttpStatus.NOT_FOUND,
             "Product with id " + id + " not found"));
     }
 
+	public Product getProductFromOpenFoodFacts(final String id) {
+		return toProduct(foodDataRepository.getProduct(id));
+	}
+
+	public Product toProduct(Map<String, Object> json) {
+		Product product = new Product();
+		//TODO save 'categories' in Product-DB
+		//TODO remove leading 0s
+		product.setId((String) json.get("code"));
+		product.setName((String) json.get("product_name"));
+		//product.setImageUrl((String) json.get("imageUrl"));
+		product.setDescription((String) json.get("labels_tags"));
+		return product;
+	}
+
+	private Object cleanValue(final Object value) {
+		if (value != null && !value.getClass().getPackageName().startsWith("java.lang")) {
+			return value.toString();
+		}
+		return value;
+	}
     public boolean hasProductBeenScanned(final String id) {
         return scannedProductRepository.existsById(id);
     }
