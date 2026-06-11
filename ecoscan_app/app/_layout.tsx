1fe847f984d851e20a7d601c242bac9db2bbc3fb
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2026-06-11 11:34:50 +0200

    :sparkles: Add product screen to root _layout

--- ecoscan_app/app/_layout.tsx
+++ ecoscan_app/app/_layout.tsx
@@ -38,6 +38,15 @@ function RootLayoutNav() {
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="(auth)" />
       <Stack.Screen name="(tabs)" />
+      <Stack.Screen
+          name="product/[id]"
+          options={{
+            headerShown: true,
+            title: "EcoScan",
+            headerStyle: { backgroundColor: theme.colors.secondary },
+            headerTitleStyle: { color: "black", fontWeight: "bold" },
+          }}
+      />
     </Stack>
   );
 }
