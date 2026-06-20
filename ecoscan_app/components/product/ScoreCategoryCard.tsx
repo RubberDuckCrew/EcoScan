618a1d484f5fd6d709499ae9249f88401c25effa
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2026-06-20 21:22:50 +0200

    :art: Format

--- ecoscan_app/components/product/ScoreCategoryCard.tsx
+++ ecoscan_app/components/product/ScoreCategoryCard.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
 import { Card, Text, ProgressBar } from "react-native-paper";
-import {getScoreVariant} from "@/utils/scoreColor";
-import {variantStyles} from "@/components/product/ScoreCard";
+import { getScoreVariant } from "@/utils/scoreColor";
+import { variantStyles } from "@/components/product/ScoreCard";
 
 interface ScoreCardProps {
   label: string;
@@ -13,19 +13,21 @@ export default function ScoreCategoryCard({ label, score }: ScoreCardProps) {
   const variant = variantStyles[getScoreVariant(score)];
 
   return (
-      <Card style={[styles.card]}>
-        <Card.Content>
-          <View style={styles.header}>
-            <Text variant="titleMedium">{label}</Text>
-            <Text variant="titleMedium" style={variant.text}>{score}</Text>
-          </View>
-          <ProgressBar
-              progress={score / 100}
-              color={variant.progressBar}
-              style={styles.progressBar}
-          />
-        </Card.Content>
-      </Card>
+    <Card style={[styles.card]}>
+      <Card.Content>
+        <View style={styles.header}>
+          <Text variant="titleMedium">{label}</Text>
+          <Text variant="titleMedium" style={variant.text}>
+            {score}
+          </Text>
+        </View>
+        <ProgressBar
+          progress={score / 100}
+          color={variant.progressBar}
+          style={styles.progressBar}
+        />
+      </Card.Content>
+    </Card>
   );
 }
 
