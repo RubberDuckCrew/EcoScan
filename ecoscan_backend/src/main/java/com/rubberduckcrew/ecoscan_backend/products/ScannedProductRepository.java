eb15ea5238bb5cee3653d6330488ddbc165bfea8
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2026-07-01 18:26:11 +0200

    :goal_net: Update getScannedProduct method to handle missing products with a proper exception

--- ecoscan_backend/src/main/java/com/rubberduckcrew/ecoscan_backend/products/ScannedProductRepository.java
+++ ecoscan_backend/src/main/java/com/rubberduckcrew/ecoscan_backend/products/ScannedProductRepository.java
@@ -1,10 +1,11 @@
 package com.rubberduckcrew.ecoscan_backend.products;
 
 import com.rubberduckcrew.ecoscan_backend.products.entity.ScannedProduct;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ScannedProductRepository extends JpaRepository<ScannedProduct, String> {
-    ScannedProduct getScannedProductById(String id);
+    Optional<ScannedProduct> getScannedProductById(String id);
 }
