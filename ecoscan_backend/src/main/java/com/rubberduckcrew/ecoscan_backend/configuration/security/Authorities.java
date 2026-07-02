8a6814e79a9b93758e8434009ab4a4432ab12e73
Friedinger <71267611+Friedinger@users.noreply.github.com>
2026-07-02 10:12:29 +0200

    :recycle: Add private constructor and final to Authorities

--- ecoscan_backend/src/main/java/com/rubberduckcrew/ecoscan_backend/configuration/security/Authorities.java
+++ ecoscan_backend/src/main/java/com/rubberduckcrew/ecoscan_backend/configuration/security/Authorities.java
@@ -1,6 +1,9 @@
 package com.rubberduckcrew.ecoscan_backend.configuration.security;
 
-public class Authorities {
+public final class Authorities {
     public static final String USER = "hasAnyRole('ecoscan-user')";
     public static final String AI = "hasAnyRole('ecoscan-ai')";
+
+    private Authorities() {
+    }
 }
