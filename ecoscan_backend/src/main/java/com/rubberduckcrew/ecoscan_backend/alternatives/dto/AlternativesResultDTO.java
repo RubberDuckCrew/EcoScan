5d5b749282b99e411f1a83fc13bdcc520e5b62b4
hm-sbartl <sophie.bartl1@hm.edu>
2026-07-02 12:29:35 +0200

    :art: Fixed Spotbugs

--- ecoscan_backend/src/main/java/com/rubberduckcrew/ecoscan_backend/alternatives/dto/AlternativesResultDTO.java
+++ ecoscan_backend/src/main/java/com/rubberduckcrew/ecoscan_backend/alternatives/dto/AlternativesResultDTO.java
@@ -3,4 +3,7 @@ package com.rubberduckcrew.ecoscan_backend.alternatives.dto;
 import java.util.List;
 
 public record AlternativesResultDTO(List<AlternativeProductDTO> alternatives) {
+    public AlternativesResultDTO {
+        alternatives = alternatives != null ? List.copyOf(alternatives) : List.of();
+    }
 }
