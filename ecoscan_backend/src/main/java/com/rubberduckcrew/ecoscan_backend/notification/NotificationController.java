6f0a5be3c62c79c494e75b23e0250370159c1048
Friedinger <71267611+Friedinger@users.noreply.github.com>
2026-07-02 10:33:15 +0200

    :passport_control: Add preauthorize to notification endpoint

--- ecoscan_backend/src/main/java/com/rubberduckcrew/ecoscan_backend/notification/NotificationController.java
+++ ecoscan_backend/src/main/java/com/rubberduckcrew/ecoscan_backend/notification/NotificationController.java
@@ -1,8 +1,10 @@
 package com.rubberduckcrew.ecoscan_backend.notification;
 
+import com.rubberduckcrew.ecoscan_backend.configuration.security.Authorities;
 import com.rubberduckcrew.ecoscan_backend.utils.AuthUtils;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +17,7 @@ public class NotificationController {
     private final NotificationSseService notificationSseService;
 
     @GetMapping("/stream")
+    @PreAuthorize(Authorities.USER)
     public SseEmitter streamNotifications() {
         final UUID userId = AuthUtils.getSub();
         return notificationSseService.createEmitter(userId);
