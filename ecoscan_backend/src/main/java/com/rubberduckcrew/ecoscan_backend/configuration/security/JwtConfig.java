ea04f51b3d5feaa87493be64863fa5205ac6de94
Friedinger <71267611+Friedinger@users.noreply.github.com>
2026-07-03 09:31:11 +0200

    :sparkles: Add jwt config

---
+++ ecoscan_backend/src/main/java/com/rubberduckcrew/ecoscan_backend/configuration/security/JwtConfig.java
@@ -0,0 +1,27 @@
+package com.rubberduckcrew.ecoscan_backend.configuration.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtValidators;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+@Configuration
+public class JwtConfig {
+    @Value("${ecoscan.security.jwk-set-uri}")
+    private String jwkSetUri;
+
+    @Value("${ecoscan.security.issuer-uri}")
+    private String issuerUri;
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        NimbusJwtDecoder decoder = NimbusJwtDecoder.withJwkSetUri(jwkSetUri).build();
+        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuerUri);
+        decoder.setJwtValidator(withIssuer);
+        return decoder;
+    }
+}
