06406813cd34807a54ee5962769f747e44abbe6a
Friedinger <71267611+Friedinger@users.noreply.github.com>
2026-07-05 11:24:58 +0200

    :wrench: Temporarily ignore alternatives from linting

--- ecoscan_app/eslint.config.js
+++ ecoscan_app/eslint.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig([
   expoConfig,
   eslintConfigPrettier,
   {
-    ignores: ["dist/*"],
+    ignores: ["dist/*", "**/alternatives/**"], // TODO: Remove alternatives
   },
   {
     rules: {
