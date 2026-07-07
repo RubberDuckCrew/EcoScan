3bc202a1762d906dd28eb6106b6124631a0598c6
hm-sbartl <sophie.bartl1@hm.edu>
2026-07-07 08:38:57 +0200

    :art: don't ignore alternatives in eslint.config.js

--- ecoscan_app/eslint.config.js
+++ ecoscan_app/eslint.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig([
   expoConfig,
   eslintConfigPrettier,
   {
-    ignores: ["dist/*", "**/alternatives/**"], // TODO: Remove alternatives
+    ignores: ["dist/*"],
   },
   {
     rules: {
